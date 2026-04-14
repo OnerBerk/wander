3ae39171232ffd57a79395863fab9262740455ef
Berk Oner <oner_berk@hotmail.com>
2026-04-14 13:35:29 +0200

    feat:clean and add workspace for types

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service.js';
 import { AppController } from './app.controller.js';
-import { PrismaModule } from './config/prisma/prisma.module.js';
 
 @Module({
-  imports: [PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
