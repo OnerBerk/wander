d4e9d1647dab216a8335a60ac9d1d16703b8b86c
Berk Oner <oner_berk@hotmail.com>
2026-04-22 15:52:56 +0200

    feat: add style to header

--- apps/web/src/components/header/header.tsx
+++ apps/web/src/components/header/header.tsx
@@ -9,15 +9,15 @@ const Header = () => {
   console.log(isDay);
 
   return (
-    <header className={`h-auto sm:h-25 p-2 ${isDay ? 'bg-[#A9CBD7]' : 'bg-[#000E38]'}`}>
-      <div className='flex justify-between'>
+    <header className={`h-auto sm:h-25 p-2 ${isDay ? 'header-day' : 'header-night'}`}>
+      <div className='font-alternate flex justify-between'>
         <div className='flex items-center gap-3'>
-          <img className={`h-15 md:h-20 ${!isDay ? 'logo-bg-day' : ''}`} src='/wander-logo.png' alt='Wander' />
-          <h1 className='text-3xl md:text-5xl font-bold'>Wander</h1>
+          <img className={`h-15 md:h-20`} src='/wander-logo.png' alt='Wander' />
+          <h1 className='text-wander-orange text-3xl md:text-5xl font-semibold'>Wander</h1>
         </div>
         <div className='flex items-center justify-between gap-2'>
           {icon && <img className='h-15 md:h-20' src={icon} alt={icon} />}
-          <p className={`text-2xl md:text-3xl ${isDay ? '' : 'text-zinc-100'}`}>{`${weather?.temperature}°C`}</p>
+          <p className={`text-2xl md:text-3xl font-semibold`}>{`${weather?.temperature}°C`}</p>
         </div>
       </div>
     </header>
