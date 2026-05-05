4cd533d45c8c7754b7b05c34f023a59477a2b5d8
Berk Oner <oner_berk@hotmail.com>
2026-05-05 20:03:00 +0200

    feat[front]:: add speedinsight

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,14 @@
 import {useRoutes} from 'react-router-dom';
+import {SpeedInsights} from '@vercel/speed-insights/react';
 import {routes} from '@/pages/routes';
 
 const App = () => {
-  return useRoutes(routes);
+  return (
+    <>
+      <SpeedInsights />
+      {useRoutes(routes)}
+    </>
+  );
 };
 
 export default App;
